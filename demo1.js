267f510c6fc13488fa389b0dae84b39680c86429
Chikmurgechetan <chikmurgechetan@gmail.com>
2022-10-19 23:13:21 -0700

    QuerySelectorL and QuerySelectorALL

--- demo1.js
+++ demo1.js
@@ -1,25 +1,23 @@
-//GET ELEMENT  BY CLASS NAME//
-/*var list=document.getElementsByClassName('list-group-item');
-console.log(list);
-console.log(list[1]);
-list[1].textContent='hello';
-list[1].style.fontWeight='bold';
-list[1].style.background='yellow';
+//QEURYSELECTOR//
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
 
-for(var i=0;i<list.length;i++)
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.visibility='hidden'; 
+
+//QEURYSELECTORALL//
+/*var secondItem=document.querySelectorAll('li:nth-child(2)');
+for(var i=0;i<secondItem.length;i++)
 {
-    list[i].style.backgroundColor='lightpink';
+    secondItem[i].style.color="green";
 }*/
-//GET ElEMENT BY TAG NAME//
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent='hello';
-li[1].style.fontWeight='bold';
-li[1].style.background='yellow';
 
-for(var i=0;i<li.length;i++)
+ 
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<odd.length; i++)
 {
-    li[i].style.backgroundColor='lightpink';
+    odd[i].style.backgroundColor = 'green';
 }
+
 
