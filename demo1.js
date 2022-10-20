a83d0e6b881cf04a21f6f9678a459224fc7499c1
Chikmurgechetan <chikmurgechetan@gmail.com>
2022-10-19 18:24:51 -0700

    ck

--- demo1.js
+++ demo1.js
@@ -1,11 +1,25 @@
-//GETELEMENT BY CLASSNAME//
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[2]);
-items[2].style.backgroundColor="green";
+//GET ELEMENT  BY CLASS NAME//
+/*var list=document.getElementsByClassName('list-group-item');
+console.log(list);
+console.log(list[1]);
+list[1].textContent='hello';
+list[1].style.fontWeight='bold';
+list[1].style.background='yellow';
 
-for(var i=0;i<items.length;i++)
+for(var i=0;i<list.length;i++)
 {
-    items[i].style.fontWeight='bold';
+    list[i].style.backgroundColor='lightpink';
+}*/
+//GET ElEMENT BY TAG NAME//
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='hello';
+li[1].style.fontWeight='bold';
+li[1].style.background='yellow';
+
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor='lightpink';
 }
 
