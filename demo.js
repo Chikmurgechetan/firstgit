141b29974a7cef113fadedd8f10989b6727e1ec0
Chikmurgechetan <chikmurgechetan@gmail.com>
2022-11-06 22:32:34 -0800

    currying in js

--- demo.js
+++ demo.js
@@ -1,2 +1,38 @@
-console.log('i love git')
-console.log('i love git hub');
+//creat currying by using bind
+ 
+let multiply=function(x,y)
+{
+    console.log(x*y);
+}
+let multiplyByTwo=multiply.bind(this,2)
+multiplyByTwo(5);
+
+let multiplyByThree=multiply.bind(this,3);
+multiplyByThree(5);
+
+//creat curring by using clouser Method
+
+/*let multiply=function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+let multiplyByTwo=multiply(2);
+multiplyByTwo(3);
+
+let multiplyByThree=multiply(3);
+multiplyByThree(10);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
